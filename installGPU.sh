b439bfb9c05aef99c3b779048444b570167c2182
Rafsanjani, Muhammod <mrz30792@gmail.com>
2020-09-08 01:06:54 +0600

    Update installGPU.sh

--- installGPU.sh
+++ installGPU.sh
@@ -1,7 +1,7 @@
 # Purge existign CUDA first
 sudo apt --purge remove 'cublas*' 'cuda*'
 sudo apt --purge remove 'nvidia*'
-sudo apt list --upgradable -a
+
 
 # Install CUDA Toolkit 10
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
