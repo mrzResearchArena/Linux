8ba5ff03feb4dd1800da857e1daf561a3d8a865d
Rafsanjani, Muhammod <mrz30792@gmail.com>
2020-09-08 01:07:26 +0600

    Update installGPU.sh

--- installGPU.sh
+++ installGPU.sh
@@ -11,6 +11,7 @@ sudo dpkg -i cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
 sudo apt update
 sudo apt install -y cuda
 
+
 # Install CuDNN 7 and NCCL 2
 wget https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64/nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
 sudo dpkg -i nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
@@ -21,10 +22,10 @@ sudo apt install -y libcudnn7 libcudnn7-dev libnccl2 libc-ares-dev
 sudo apt autoremove
 sudo apt upgrade
 
+
 # Link libraries to standard locations
 sudo mkdir -p /usr/local/cuda-10.0/nccl/lib
 sudo ln -s /usr/lib/x86_64-linux-gnu/libnccl.so.2 /usr/local/cuda/nccl/lib/
 sudo ln -s /usr/lib/x86_64-linux-gnu/libcudnn.so.7 /usr/local/cuda-10.0/lib64/
 
 echo 'If everything worked fine, reboot now.'
-
